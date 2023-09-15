51fca9bceca09aa91de15e4a09881af4bb275586
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-15 11:49:42 +0000

    Sep 15, 2023, 5:19 PM

---
+++ Book/Let Us Java/Chapter - 2/question_1.java
@@ -0,0 +1,11 @@
+public class question_1 {
+    public static void main(String[] args) {
+        float p, r, si;
+        int n;
+        p = 1000.50f;
+        n = 3;
+        r = 15.5f;
+        si = (p * n * r) / 100;
+        System.out.println(si);
+    }
+}
