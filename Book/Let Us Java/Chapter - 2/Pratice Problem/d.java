c4c073043a8d3b452317c8d3abf1575f79a18836
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-15 14:05:07 +0000

    Sep 15, 2023, 7:35 PM

--- Book/Let Us Java/Chapter - 2/Pratice Problem/d.java
+++ Book/Let Us Java/Chapter - 2/Pratice Problem/d.java
@@ -1,6 +1,8 @@
 public class d {
     public static void main(String[] args) {
         float fahrenheit = 98.4f;
-        
+        float celsius = (fahrenheit - 32) * 5 / 9;
+        System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+        System.out.println("Temperature in Celsius: " + celsius);
     }
 }
