4d250b67d6a14e07de747d948f96bab5c8de7705
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-15 12:25:13 +0000

    Sep 15, 2023, 5:55 PM

--- Book/Let Us Java/Chapter - 2/Pratice Problem/metricConversion.java
+++
@@ -1,8 +0,0 @@
-public class metricConversion {
-    public static void main(String[] args) {
-        float cityDistance = 2.5f;
-        float meter = cityDistance * 1000;
-        float feet = cityDistance * 3280.84f;
-        float inches = cityDistance * 39370.1f;
-    }
-}
---
+++ Book/Let Us Java/Chapter - 2/Pratice Problem/question_b.java
@@ -0,0 +1,13 @@
+public class question_b {
+    public static void main(String[] args) {
+        float cityDistance = 2.5f;
+        float meter = cityDistance * 1000;
+        float feet = cityDistance * 3280.84f;
+        float inches = cityDistance * 39370.1f;
+        float centimeter = cityDistance * 100000;
+        System.out.println("City Distance in Meter: " + meter);
+        System.out.println("City Distance in Feet: " + feet);
+        System.out.println("City Distance in Inches: " + inches);
+        System.out.println("City Distance in Centimeter: " + centimeter);
+    }
+}
