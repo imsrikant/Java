4855a1f6e8b3e138dfa7b968add0769d68790357
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-17 08:38:58 +0000

    Sep 17, 2023, 2:08 PM

--- Book/Let Us Java/Chapter - 4/a.java
+++ Book/Let Us Java/Chapter - 4/a.java
@@ -4,13 +4,20 @@ import java.util.Scanner;
 public class a {
 
     public static void main(String[] args) {
-        float cost, selling, profit, loss;
+        float cost, selling, profitPercentage, lossPercentage;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter cost of item: ");
         cost = sc.nextFloat();
         System.out.print("Enter selling price of item: ");
         selling = sc.nextFloat();
-        
+        if (selling > cost) {
+            profitPercentage = ((selling - cost) / cost) * 100;
+            System.out.println("Profit: " + profitPercentage + "%");
+        }
+        else {
+            lossPercentage = ((cost - selling) / cost) * 100;
+            System.out.println("Loss: " + lossPercentage + "%");
+        }
         sc.close();
     }
 }
