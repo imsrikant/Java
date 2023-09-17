aa6c3a88e150c6523d5095cf479e5db56694027c
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-17 08:34:22 +0000

    Sep 17, 2023, 2:04 PM

---
+++ Book/Let Us Java/Chapter - 4/a.java
@@ -0,0 +1,16 @@
+// input cost of item and selling price of item and find profit or loss
+import java.util.Scanner;
+
+public class a {
+
+    public static void main(String[] args) {
+        float cost, selling, profit, loss;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter cost of item: ");
+        cost = sc.nextFloat();
+        System.out.print("Enter selling price of item: ");
+        selling = sc.nextFloat();
+        
+        sc.close();
+    }
+}
