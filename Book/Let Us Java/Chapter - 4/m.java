0ec85d5265025ee819e9d0dc2b470d53a1431134
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-18 08:30:02 +0000

    Sep 18, 2023, 2:00 PM

--- Book/Let Us Java/Chapter - 4/m.java
+++ Book/Let Us Java/Chapter - 4/m.java
@@ -1,1 +1,23 @@
-// 
+// character entered is character, symbol or digit
+
+import java.util.Scanner;
+
+public class m {
+    public static void main(String[] args) {
+        char ch;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        ch = sc.nextLine().charAt(0);
+         if (ch >= 97 && ch <= 123) {
+            System.out.println("Character is lowercase");
+        }
+        else if (ch >= 48 && ch <= 57) {
+            System.out.println("Character is digit");
+        }
+        else {
+            System.out.println("Character is symbol");
+        }
+
+        sc.close();
+    }
+}
