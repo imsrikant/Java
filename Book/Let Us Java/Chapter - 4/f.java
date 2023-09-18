7d5e182907f60e821db8738c1520440583fc5f24
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-18 08:15:44 +0000

    Sep 18, 2023, 1:45 PM

--- Book/Let Us Java/Chapter - 4/f.java
+++ Book/Let Us Java/Chapter - 4/f.java
@@ -1,1 +1,21 @@
 // to check the triangle is valid or not using its sides
+
+import java.util.Scanner;
+public class f {
+    public static void main(String[] args) {
+        float sideA, sideB, sideC;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first side of triangle: ");
+        sideA = sc.nextFloat();
+        System.out.print("Enter the second side of triangle: ");
+        sideB = sc.nextFloat();
+        System.out.print("Enter the third side of triangle: ");
+        sideC = sc.nextFloat();
+        if ((sideA + sideB) > sideC && (sideB + sideC) > sideA && (sideC + sideA) > sideB) {
+            System.out.println("Triangle is valid");
+        } else {
+            System.out.println("Triangle is not valid");
+        }
+        sc.close();
+    }
+}
