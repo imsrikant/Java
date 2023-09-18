1a4ea7c67fad0c533376b073bef847ef112db423
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-18 08:21:23 +0000

    Sep 18, 2023, 1:51 PM

--- Book/Let Us Java/Chapter - 4/h.java
+++ Book/Let Us Java/Chapter - 4/h.java
@@ -1,5 +1,21 @@
 // rectangle area is greater than its perimeter
 
+import java.util.Scanner;
 public class h {
-    
+    public static void main(String[] args) {
+        float length, breadth, area, perimeter;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of rectangle: ");
+        length = sc.nextFloat();
+        System.out.print("Enter the breadth of rectangle: ");
+        breadth = sc.nextFloat();
+        area = length * breadth;
+        perimeter = 2 * (length + breadth);
+        if (area > perimeter) {
+            System.out.println("Area is greater than perimeter");
+        } else {
+            System.out.println("Area is not greater than perimeter");
+        }
+        sc.close();
+    }
 }
