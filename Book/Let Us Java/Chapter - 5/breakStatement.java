a4d784f044e68269a7de4b0c8e1c004ad02ba9ba
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-19 07:13:48 +0000

    Sep 19, 2023, 12:43 PM

---
+++ Book/Let Us Java/Chapter - 5/breakStatement.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class breakStatement {
+    public static void main() {
+        int num, i;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        num = sc.nextInt();
+        i = 2;
+        while (i <= num - 1) {
+            if (num % i == 0) {
+                System.out.println("Not a prime number");
+                break;
+            }
+            i++;
+        }
+        sc.close();
+    }
+}
