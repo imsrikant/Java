bff1a73575acb8f88bc43c1ff9f9169e37dbff96
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-20 01:22:57 +0000

    Sep 20, 2023, 6:52 AM

--- Book/Let Us Java/Chapter - 7/c.java
+++ Book/Let Us Java/Chapter - 7/c.java
@@ -10,13 +10,17 @@ public class c {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the year: ");
         year = sc.nextInt();
-
+        boolean isTrue = isLeap(year);
+        if (isTrue) {
+            System.out.println("Leap");
+        } else {
+            System.out.println("Not Leap");
+        }
         sc.close();
     }
 
     public static boolean isLeap(int year) {
         boolean isLeap = false;
-
         if (year % 4 == 0) {
             if (year % 100 == 0) {
                 if (year % 400 == 0) {
