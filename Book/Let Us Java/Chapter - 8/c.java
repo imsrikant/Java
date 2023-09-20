333c41ef9af93ae719fe78179b6a9fd88904452d
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-20 13:11:08 +0000

    Sep 20, 2023, 6:41 PM

---
+++ Book/Let Us Java/Chapter - 8/c.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * c
+ */
+public class c {
+
+    public static void main(String[] args) {
+        int number, sum;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number up to five digits: ");
+        number = scanner.nextInt();
+
+        scanner.close();
+    }
+
+    static int getSum(int number) {
+        int sum = 0;
+        if (number == 0) {
+            return 0;
+        } else {
+            sum = sum + getSum(number - 1);
+        }
+        return sum;
+    }
+}
