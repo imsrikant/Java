d4682d6b7eb11950069110b9e765c4e6cb70433b
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-20 12:42:05 +0000

    Sep 20, 2023, 6:12 PM

--- Book/Let Us Java/Chapter - 8/a.java
+++ Book/Let Us Java/Chapter - 8/a.java
@@ -5,6 +5,26 @@
 import java.util.Scanner;
 public class a {
     public static void main(String[] args) {
-        
+        int intA = 5, intB = 54, intMax;
+        double doubleA = 5.9999644, doubleB = 5.54455654, doubleMax;
+        float floatA = 5.46545f, floatB = 5.4654f, floatMax;
+        intMax = max(intA, intB);
+        floatMax = max(floatA, floatB);
+        doubleMax = max(doubleA, doubleB);
+        System.out.println("Max int " + intMax);
+        System.out.println("Max float " + floatMax);
+        System.out.println("Max double " + doubleMax);
+    }
+
+    static int max(int intA, int intB) {
+        return intA > intB ? intA : intB;
+    }
+
+    static float max(float floatA, float floatB) {
+        return floatA > floatB ? floatA : floatB;
+    }
+
+    static double max(double doubleA, double doubleB) {
+        return doubleA > doubleB ? doubleA : doubleB;
     }
 }
