bef7c723b48153f4ad2ec057a654d8407d126e44
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-20 12:48:43 +0000

    Sep 20, 2023, 6:18 PM

---
+++ Book/Let Us Java/Chapter - 8/b.java
@@ -0,0 +1,16 @@
+/**
+ * b
+ */
+import java.util.Scanner;
+public class b {
+    public static void main(String[] args) {
+
+    }
+
+    static int min(int... number) {
+        
+        return 0;
+
+    }
+
+}
