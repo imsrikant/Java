b703597b970256c7c6ba91b59f18da76e9d3bf3c
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-20 12:51:59 +0000

    Sep 20, 2023, 6:21 PM

--- Book/Let Us Java/Chapter - 8/b.java
+++ Book/Let Us Java/Chapter - 8/b.java
@@ -4,13 +4,15 @@
 import java.util.Scanner;
 public class b {
     public static void main(String[] args) {
-
+        System.out.println("Minimum: " + min(1, 2, 3, 45, 57, 0, -99));
     }
 
     static int min(int... number) {
-        
-        return 0;
-
+        int min = 0;
+        for (int i : number) {
+            min = min < i ? min : i;
+        }
+        return min;
     }
 
 }
