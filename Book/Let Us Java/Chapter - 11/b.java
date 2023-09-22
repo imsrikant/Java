17cbef8d57aa4443f42a4e8feeb811720817e3ae
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-22 13:13:30 +0000

    Sep 22, 2023, 6:43 PM

--- Book/Let Us Java/Chapter - 11/b.java
+++ Book/Let Us Java/Chapter - 11/b.java
@@ -1,2 +1,48 @@
 // twenty five number to input check how many times positive, negative, even and odd is present
 
+/**
+ * b
+ */
+import java.util.Scanner;
+
+public class b {
+
+    public static void main(String[] args) {
+        int[] arr;
+        arr = new int[25];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("Enter the element %d: ", (i + 1));
+            arr[i] = scanner.nextInt();
+        }
+        scanner.close();
+    }
+
+    static void evenAndOdd(int[] arr) {
+        int countEven = 0, countOdd = 0;
+        for (int arrElement : arr) {
+            if (arrElement % 2 == 0) {
+                countEven++;
+            } else {
+                countOdd++;
+            }
+        }
+        System.out.println("Even: " + countEven);
+        System.out.println("Odd: " + countOdd);
+    }
+
+    static void positiveOrNot(int[] arr) {
+        int countPositive = 0, countNegative = 0;
+        for (int arrElement : arr) {
+            if (arrElement > 0) {
+                countPositive++;
+            } else if (arrElement == 0) {
+                continue;
+            } else {
+                countNegative++;
+            }
+        }
+        System.out.println("Positive: " + countPositive);
+        System.out.println("Negative: " + countNegative);
+    }
+}
