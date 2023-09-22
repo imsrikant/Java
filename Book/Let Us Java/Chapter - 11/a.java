83b1f585ed17b55450982e4cfe50ac81872e1e06
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-22 12:53:14 +0000

    Sep 22, 2023, 6:23 PM

--- Book/Let Us Java/Chapter - 11/a.java
+++ Book/Let Us Java/Chapter - 11/a.java
@@ -1,1 +1,45 @@
-// twenty five element search a value in
+
+// twenty five element search a value and how many times it repeated
+/**
+ * a
+ */
+import java.util.Scanner;
+
+public class a {
+
+    public static void main(String[] args) {
+        double[] arr;
+        arr = new double[25];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter the element " + (i + 1) + ": ");
+            arr[i] = scanner.nextDouble();
+        }
+        System.out.print("Enter the value for search: ");
+        double value = scanner.nextDouble();
+        System.out.printf("The number of time value is present is %i.", (howManyTimes(arr, value)));
+        scanner.close();
+    }
+
+    static int howManyTimes(double[] arr, double value) {
+        int count = 0;
+        if (isPresent(arr, value)) {
+            for (double arrElement : arr) {
+                if (arrElement == value) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    static boolean isPresent(double[] arr, double value) {
+        boolean isTrue = false;
+        for (double arrElement : arr) {
+            if (arrElement == value) {
+                return isTrue = true;
+            }
+        }
+        return isTrue;
+    }
+}
