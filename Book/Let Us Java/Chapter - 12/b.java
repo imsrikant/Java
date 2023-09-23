394ea710ae98f0798b3eaf35aa26a581d30d68b6
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-23 13:45:00 +0000

    Sep 23, 2023, 7:15 PM

--- Book/Let Us Java/Chapter - 12/b.java
+++ Book/Let Us Java/Chapter - 12/b.java
@@ -1,2 +1,30 @@
 // reverse the string stored in array
+/**
+ * b
+ */
+public class b {
 
+    public static void main(String[] args) {
+        String str[] = {
+                "To err is human",
+                "This is a cat",
+                "This is a dog"
+        };
+        printStr(str);
+        reverseStrArr(str);
+        printStr(str);
+
+    }
+
+    public static void reverseStrArr(String[] str) {
+        for (int i = 0; i < str.length; i++) {
+            str[i] = new StringBuilder(str[i]).reverse().toString();
+        }
+    }
+
+    public static void printStr(String[] str) {
+        for (String string : str) {
+            System.out.println(string);
+        }
+    }
+}
