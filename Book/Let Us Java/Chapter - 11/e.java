f18d04f22e1d78aac19f4772c38dcba25cdb0014
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-23 07:34:43 +0000

    Sep 23, 2023, 1:04 PM

--- Book/Let Us Java/Chapter - 11/e.java
+++ Book/Let Us Java/Chapter - 11/e.java
@@ -8,7 +8,45 @@ public class e {
 
     public static void main(String[] args) {
         final int size = 4;
-        int[][] arr = new int[size][size];
-        
+        int[][] arr = give2dArr(size, size);
+        enter2dValue(arr);
+        System.out.println("Array.");
+        print2dArr(arr);
+        System.out.println("Transpose Array.");
+        transpose2dArr(arr);
     }
+
+    static int[][] give2dArr(int rows, int columns) {
+        int[][] temp = new int[rows][columns];
+        return temp;
+    }
+
+    static void enter2dValue(int[][] arr) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.printf("Enter the value for row %d X column %d: ", i, j);
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+        scanner.close();
+    }
+    static void transpose2dArr(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.printf("%d ", arr[j][i]);
+            }
+            System.out.println();
+        }
+    }
+    static void print2dArr(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.printf("%d ", arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+
 }
