452322a49cb6e8149104a9cd7c15479e8db4d39c
Srikant Kumar <52560067+srikant-marvel@users.noreply.github.com>
2023-09-23 13:51:36 +0000

    Sep 23, 2023, 7:21 PM

---
+++ Book/Let Us Java/Chapter - 12/c.java
@@ -0,0 +1,20 @@
+/**
+ * c
+ */
+import java.util.Scanner;
+public class c {
+
+    public static void main(String[] args) {
+        String str;
+        final String strDel = "this";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a sentence with the: ");
+        str = scanner.next();
+        StringBuilder str1 = new StringBuilder(str);
+        scanner.close();
+    }
+
+    public static void name() {
+        
+    }
+}
